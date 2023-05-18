7ad45f3425f278e17920350fa2cc0676bae14553
FarhanSadeek21 <farhansadeek019@gmail.com>
2023-05-18 11:55:05 -0400

    Initial Commit

---
+++ src/GamePanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class GamePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.green);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+    }
+}
